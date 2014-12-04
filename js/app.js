0ac944b72fea92313f5a8a955fba1ebcef8cb562
Magda Ang <angmagda@gmail.com>
2014-12-04 14:42:27 -0700

    change images

--- js/app.js
+++ js/app.js
@@ -1,53 +1,53 @@
 var app = angular.module('productIdGen', ['ui.router', 'ngRoute'])
 
-.config(function($stateProvider, $urlRouterProvider) {
+// .config(function($stateProvider, $urlRouterProvider) {
 	
 
-	$urlRouterProvider.otherwise('/');
+// 	$urlRouterProvider.otherwise('/');
 
-	$stateProvider
+// 	$stateProvider
 
 	// HOME STATES AND NESTED VIEWS
 
-		.state('home', {
-			url: '/',
-			templateUrl: 'views/partial-home.html',
-			controller: 'MainController'
-		})
+		// .state('home', {
+		// 	url: '/',
+		// 	templateUrl: 'views/partial-home.html',
+		// 	controller: 'MainController'
+		// })
 
 		// Nested list
-	    .state('home.list', {
-	        url: '/list',
-	        templateUrl: 'views/partial-home-list.html',
-	        controller: 'MainController'
-	    })
+	    // .state('home.list', {
+	    //     url: '/list',
+	    //     templateUrl: 'views/partial-home-list.html',
+	    //     controller: 'MainController'
+	    // })
 
 
 	    // nested list with just some random string data
-	    .state('home.paragraph', {
-	        url: '/paragraph',
-	        template: 'Whaaaaaaaatt?'
-	    })
+	 //    .state('home.paragraph', {
+	 //        url: '/paragraph',
+	 //        template: 'Whaaaaaaaatt?'
+	 //    })
 
 
 
-		.state('generator', {
-			url:'/generator',
-			templateUrl: 'views/generator.html',
-			controller: 'MainController'
-		})
+		// .state('generator', {
+		// 	url:'/generator',
+		// 	templateUrl: 'views/generator.html',
+		// 	controller: 'MainController'
+		// })
 
-		.state('products', {
-			url:'/products',
-			templateUrl: 'views/products.html',
-			controller: 'MainController'
-		})
+// 		.state('products', {
+// 			url:'/products',
+// 			templateUrl: 'views/products.html',
+// 			controller: 'MainController'
+// 		})
 
-		.state('contact',{
-			url: '/contact',
-			templateUrl: 'views/contact.html',
-			controller: 'MainController'
+// 		.state('contact',{
+// 			url: '/contact',
+// 			templateUrl: 'views/contact.html',
+// 			controller: 'MainController'
 
-		});
+// 		});
 
-});
+// });
