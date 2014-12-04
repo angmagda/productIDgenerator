8edec58e630fc827a68e6cd330d7ebd49269f93c
Magda Ang <angmagda@gmail.com>
2014-12-04 12:34:01 -0700

    Visual improvement

---
+++ js/MainController.js
@@ -0,0 +1,53 @@
+var app = angular.module('productIdGen');
+
+app.controller('MainController', function ($scope, mainService) {
+
+	$scope.test = 'This is a test';
+	$scope.products = ['Finials', 'Brackets', 'Masters', 'Carriers'];
+	$scope.product = {
+	
+		// productName:
+		// manufacturer:
+		// manufacturerId
+		// material:
+		// dimensions:
+		// color:
+		// newProductId:
+
+	};
+
+
+
+	$scope.getData = function() {
+
+		mainService.getData().then(function(res) {
+			$scope.categories = res.data.categories;
+			$scope.manufacturers = res.data.manufacturers;
+			$scope.materials = res.data.materials;
+			$scope.colors = res.data.colors;
+		})
+	}	
+
+	$scope.addProductId = function() {
+		// $scope.product.category = $scope.product.category.name;
+		// $scope.product.productName = $scope.product.productName;
+		// $scope.product.manufacturer = $scope.product.manufacturer.name;
+		mainService.addProductId($scope.product, function (data){
+			//$scope.product = data.data;
+		});
+	}
+
+
+	$scope.getData(); // can put this in my generator state in the resolve
+
+	// $scope.getManufacturers = function() {
+
+	// 	mainService.getManufacturers().then(function(data) {
+	// 		$scope.manufacturers = data.data;
+	// 		// console.log(data.data)
+	// 	})
+	// }
+	// $scope.getManufacturers();
+
+
+});
---
+++ js/mainService.js
@@ -0,0 +1,49 @@
+var app = angular.module('productIdGen');
+
+app.service('mainService', function ($http) {
+	this.getData = function() {
+		return $http({
+			method: 'GET',
+			url: 'http://localhost:3000/generator'
+		})
+
+	},
+
+// 	this.addProductId = function(product, $scope) {
+// 		return $http({
+// 			method: 'POST',
+// 			url: 'http://localhost:3000/products',
+// 			data: {}
+// 		}).then(function(newProductId){
+// 			return newProductId.data
+// 		});
+		
+// }
+
+//  this.addProductId = function(product, callback) {
+//    console.log('new product function')
+//    return $http.post('http://localhost:3000/products', {product: product})
+//    .success(function(data, status, headers, config){
+//        console.log(data);
+//        callback(data);
+//    })
+//        .error(function(data, status, headers, config) {
+//            console.log(status);
+//        })
+// }
+
+this.addProductId = function(product, callback) {
+   return $http( {
+   		method: 'POST',
+   		url: 'http://localhost:3000/products', 
+   		data: {product: product}
+   })
+   .success(function(data, status, headers, config){
+       console.log(data, 'working');
+   })
+       .error(function(data, status, headers, config) {
+           console.log(status);
+       })
+}
+
+	})
